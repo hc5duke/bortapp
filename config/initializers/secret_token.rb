3fcf4b3f0c0fa38903aef99ba4b8455f424ff07a
Hwan-Joon Choi <hc5duke@gmail.com>
2011-05-23 17:54:17 -0700

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BortApp::Application.config.secret_token = '20f4d523a23b1097915e6bb5ab8d8ea3e2f47a14792b7c7ab5b73e66236550f967fb8979a5539a143b84124fbd8bbb0c2d7c1197c735dec4caf62530931fbfb1'
