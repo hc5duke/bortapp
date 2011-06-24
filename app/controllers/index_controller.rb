a65f1fa59019e0f5e2ea74a1dc1b60590512a2ff
Hwan-Joon Choi <hc5duke@gmail.com>
2011-06-23 17:41:53 -0700

    morning and evening

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,5 +1,28 @@
 class IndexController < ApplicationController
   def index
+    if params[:morning]
+      hour = params[:morning] == 'true' ? 0 : 23
+    elsif params[:evening]
+      hour = params[:evening] == 'true' ? 23 : 0
+    else
+      hour = Time.now.in_time_zone('Pacific Time (US & Canada)').hour
+    end
+    hour < 12 ? morning : evening
+  end
+
+private
+  def morning
+    @stations = %w(dubl).map do |abbreviation|
+      Bort::Realtime::Estimates.new(abbreviation)
+    end
+
+    @destinations = @stations.map do |station|
+      station.destinations
+    end.flatten.uniq
+  end
+
+  def evening
+    @is_evening = true
     @stations = %w(16th civc powl mont embr woak).map do |abbreviation|
       Bort::Realtime::Estimates.new(abbreviation)
     end
@@ -7,11 +30,5 @@ class IndexController < ApplicationController
     @destinations = @stations.map do |station|
       station.destinations
     end.flatten.uniq - %w(PITT FRMT RICH SHAY DUBL CONC NCON MONT)
-
-    @max_length = @stations.map do |station|
-      station.trains.select do |train|
-        @destinations.include?(train.destination)
-      end.map(&:minutes).length
-    end.max
   end
 end
